f5489d06e9e70277ef9cff25f537121e676d59c8
Will Daly <will.e.daly@gmail.com>
2019-08-25 16:15:52 -0700

    Small wording change to initial devlog message
    Use consistent phrasing for the different task types.

--- src/repository.rs
+++ src/repository.rs
@@ -11,7 +11,7 @@ const HELP_MSG: &'static str = "Welcome to your devlog!
 
 You can add tasks below using this format:
 * Use an asterisk (*) for each task you want to complete today.
-^ Use a caret symbol (^) for each task that's in progress.
+^ Use a caret symbol (^) for tasks that are in progress.
 + Use a plus sign (+) for tasks you completed
 - Use a minus sign (-) for tasks that are blocked.
 
